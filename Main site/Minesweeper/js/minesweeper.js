49f178922e3db05597f1a73fd05fb8a44da1887a
Caden Henrich <henrich.rocking@gmail.com>
2019-01-30 08:39:03 -0500

    Formatting
    Fixed formatting issues and errors in minesweeper.js

--- Main site/Minesweeper/js/minesweeper.js
+++ Main site/Minesweeper/js/minesweeper.js
@@ -1,12 +1,12 @@
 const FPS = 60;
 
-var canv = document.getElementById("sweepcanv");
-var ctx = canv.getContext("2d");
+var canv = document.getElementById('sweepcanv');
+var ctx = canv.getContext('2d');
 
 var cw = canv.width;
 var ch = canv.height;
 
-document.addEventListener("click", click);
+document.addEventListener('click', click);
 
 var bannerHeight = 20;
 var buffer = 5;
@@ -34,7 +34,7 @@ while (bombsLeft > 0) {
         grid[i][j] = newGridObj(
           j * playArea.bw,
           i * playArea.bh,
-          "bomb",
+          'bomb',
           0
         );
         bombsLeft--;
@@ -42,7 +42,7 @@ while (bombsLeft > 0) {
         grid[i][j] = newGridObj(
           j * playArea.bw,
           i * playArea.bh,
-          "clear",
+          'clear',
           0
         );
       }
@@ -79,97 +79,100 @@ function newGridObj(x, y, id, surrounding) {
     id: id,
     surrounding: surrounding,
     clicked: false,
-    draw: function() {
+    draw: function () {
       if (!this.clicked) {
-        ctx.strokeStyle = "Black";
+        ctx.strokeStyle = 'Black';
         ctx.lineWidth = 1;
         ctx.strokeRect(this.x, this.y, playArea.bw, playArea.bh);
 
-        ctx.fillStyle = "Gray";
+        ctx.fillStyle = 'Gray';
         ctx.fillRect(this.x, this.y, playArea.bw, playArea.bh);
 
-        ctx.fillStyle = "Gainsboro";
+        ctx.fillStyle = 'Gainsboro';
         ctx.fillRect(this.x + 3, this.y + 3, playArea.bw - 3 * 2, playArea.bh - 3 * 2);
       } else {
-        ctx.strokeStyle = "Black";
+        ctx.strokeStyle = 'Black';
         ctx.lineWidth = 1;
         ctx.strokeRect(this.x, this.y, playArea.bw, playArea.bh);
 
-        ctx.fillStyle = "White";
+        ctx.fillStyle = 'White';
         ctx.fillRect(this.x, this.y, playArea.bw, playArea.bh);
 
-        ctx.strokeStyle = "Black";
-        switch(this.surrounding) {
+        ctx.strokeStyle = 'Black';
+        switch (this.surrounding) {
           case 1:
-            ctx.fillStyle = "Blue";
+            ctx.fillStyle = 'Blue';
             break;
           case 2:
-            ctx.fillStyle = "Green";
+            ctx.fillStyle = 'Green';
             break;
           case 3:
-            ctx.fillStyle = "Red";
+            ctx.fillStyle = 'Red';
             break;
           case 4:
-            ctx.fillStyle = "Purple";
+            ctx.fillStyle = 'Purple';
             break;
           case 5:
-            ctx.fillStyle = "Maroon";
+            ctx.fillStyle = 'Maroon';
             break;
           case 6:
-            ctx.fillStyle = "Cyan";
+            ctx.fillStyle = 'Cyan';
             break;
           case 7:
-            ctx.fillStyle = "Yellow";
+            ctx.fillStyle = 'Yellow';
             break;
           case 8:
-            ctx.fillStyle = "Black";
+            ctx.fillStyle = 'Black';
             break;
           default:
-            ctx.fillStyle = "White";
+            ctx.fillStyle = 'White';
             break;
         }
-        if (this.surrounding !== 0 && this.id !== "bomb") {
-          ctx.font = "" + playArea.bw + "px Helvetica";
+        if (this.surrounding !== 0 && this.id !== 'bomb') {
+          ctx.font = '' + playArea.bw + 'px Helvetica';
           ctx.fillText(this.surrounding, this.x + playArea.bw / 4, this.y + playArea.bh - buffer);
         } else if (this.surrounding !== 0) {
-          ctx.font = "" + playArea.bw + "px Helvetica";
-          ctx.fillText("B", this.x + playArea.bw / 4, this.y + playArea.bh - buffer);
+          ctx.font = '' + playArea.bw + 'px Helvetica';
+          ctx.fillText('B', this.x + playArea.bw / 4, this.y + playArea.bh - buffer);
         }
       }
     },
-    findSurrounding: function(array, index1, index2, search) {
+
+    findSurrounding: function (array, index1, index2, search) {
       bReturn = 0;
       for (var i = -1; i < 2; i++) {
         for (var j = -1; j < 2; j++) {
           try {
-            if (array[index1 - i][index2 - j].id === "bomb") {
+            if (array[index1 - i][index2 - j].id === 'bomb') {
               bReturn++;
               if (search) {
-                if (0 === array[index1 - i][index2 - j].findSurrounding(array, index1, index2, false)) {
+                if (array[index1 - i][index2 - j].findSurrounding(
+                  array, index1, index2, false) === 0) {
                   array[index1 - i][index2 - j].clicked = true;
                   array[index1 - i][index2 - j].findSurrounding(array, index1, index2, true);
                 }
               }
             }
           }
-          catch(error) {
+          catch (error) {
             console.error(error);
           }
         }
       }
+
       this.surrounding = bReturn;
-      return bReturn;
       console.log(bReturn);
-    }
+      return bReturn;
+    },
   };
 }
 
 function update() {
-  ctx.fillStyle = "White";
+  ctx.fillStyle = 'White';
   ctx.fillRect(0, 0, cw, ch);
 
   for (var i = 0; i < grid.length; i++) {
-    for (var j =0; j < grid[i].length; j++) {
+    for (var j = 0; j < grid[i].length; j++) {
       grid[i][j].draw();
     }
   }
