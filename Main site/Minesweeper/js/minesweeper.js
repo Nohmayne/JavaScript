2d5ec78f8ba73906eb8a32022048514abc450e73
Caden Henrich <henrich.rocking@gmail.com>
2019-02-05 00:13:50 -0500

    Restructured bomb placement (MS)

--- Main site/Minesweeper/js/minesweeper.js
+++ Main site/Minesweeper/js/minesweeper.js
@@ -22,33 +22,53 @@ var playArea = {
 };
 playArea.bw = playArea.width / playArea.gw;
 playArea.bh = playArea.height / playArea.gh;
-var bombNum = 99;
-var bombsLeft = bombNum;
+var bombNum = 100;
+var bPosp = [];
 var grid = [];
 
-while (bombsLeft > 0) {
-  for (var i = 0; i < playArea.gh; i++) {
-    grid[i] = [];
-    for (var j = 0; j < playArea.gw; j++) {
-      if (getRandomInt(0, playArea.gw) <= 1) {
-        grid[i][j] = newGridObj(
-          j * playArea.bw,
-          i * playArea.bh,
-          'bomb',
-          0
-        );
-        bombsLeft--;
-      } else {
-        grid[i][j] = newGridObj(
-          j * playArea.bw,
-          i * playArea.bh,
-          'clear',
-          0
-        );
-      }
-    }
+for (var i = 0; i < playArea.gh; i++) {
+  grid[i] = [];
+  for (var j = 0; j < playArea.gw; j++) {
+    grid[i][j] = newGridObj(
+      j * playArea.bw,
+      i * playArea.bh,
+      'clear',
+      0
+    );
   }
 }
+
+for (var b = 0; b < bombNum; b++) {
+  placed = false;
+  nPos = {
+    x: getRandomInt(0, playArea.gw - 1),
+    y: getRandomInt(0, playArea.gh - 1),
+  };
+  filled = false;
+  for (var n = 0; n < bPosp.length; n++) {
+    if (bPosp[n] === nPos) {
+      filled = true;
+    }
+  }
+
+  if (!filled) {
+    grid[nPos.y][nPos.x] = [];
+    grid[nPos.y][nPos.x] = newGridObj(
+      nPos.x * playArea.bw,
+      nPos.y * playArea.bh,
+      'bomb',
+      0
+    );
+    bPosp.push(nPos);
+    placed = true;
+  }
+
+  if (!placed) {
+    b--;
+  }
+}
+
+console.log(grid);
 
 for (var p = 0; p < grid.length; p++) {
   for (var q = 0; q < grid[p].length; q++) {
@@ -87,6 +107,7 @@ function newGridObj(x, y, id, surrounding) {
     id: id,
     surrounding: surrounding,
     clicked: false,
+    flagged: false,
     draw: function () {
       if (!this.clicked) {
         ctx.strokeStyle = 'Black';
@@ -98,6 +119,12 @@ function newGridObj(x, y, id, surrounding) {
 
         ctx.fillStyle = 'Gainsboro';
         ctx.fillRect(this.x + 3, this.y + 3, playArea.bw - 3 * 2, playArea.bh - 3 * 2);
+
+        if (this.flagged) {
+          ctx.fillStyle = 'Red';
+          ctx.font = '' + playArea.bw + 'px Helvetica';
+          ctx.fillText('F', this.x + playArea.bw / 4, this.y + playArea.bh - buffer);
+        }
       } else {
         ctx.strokeStyle = 'Black';
         ctx.lineWidth = 1;
@@ -143,6 +170,7 @@ function newGridObj(x, y, id, surrounding) {
           ctx.font = '' + playArea.bw + 'px Helvetica';
           ctx.fillText(this.surrounding, this.x + playArea.bw / 4, this.y + playArea.bh - buffer);
         } else if (this.surrounding !== 0) {
+          ctx.fillStyle = 'White';
           ctx.font = '' + playArea.bw + 'px Helvetica';
           ctx.fillText('B', this.x + playArea.bw / 4, this.y + playArea.bh - buffer);
         }
